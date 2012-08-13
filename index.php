1d59c404bd415d5b8a7b1f2431562848ae45f057
Rajinder <rajinder.think360@gmail.com>
2012-08-13 03:10:55 -0700

    committed a file nothing else

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+	include_once("header-short.php");
+?>
+<div id="header">
+	<div id="header-inner">
+    	<h1><a href="index.php"></a></h1> <!-- /Logo -->
+        <div class="fright">
+        	<a href="request.php" class="btn5"><span>Request An Invite</span></a>
+            <a href="login.php" class="btn6 space3"><span>Login</span></a>
+        </div>
+    </div>
+</div>
+
+<div class="lp-container">
+	<div class=" lp-img"></div>
+    <div class="lp-frnd-main">
+    	<div class="heading-tag">People In Your City</div>
+        <ul class="lp-frnd-list">
+            <li><a href="#"><img src="img/img-60x60-1.jpg" alt="" /></a></li>
+            <li><a href="#"><img src="img/img-60x60-1.jpg" alt="" /></a></li>
+            <li><a href="#"><img src="img/img-60x60-1.jpg" alt="" /></a></li>
+            <li><a href="#"><img src="img/img-60x60-1.jpg" alt="" /></a></li>
+            <li><a href="#"><img src="img/img-60x60-1.jpg" alt="" /></a></li>
+            <li><a href="#"><img src="img/img-60x60-1.jpg" alt="" /></a></li>
+            <li><a href="#"><img src="img/img-60x60-1.jpg" alt="" /></a></li>
+            <li><a href="#"><img src="img/img-60x60-1.jpg" alt="" /></a></li>
+            <li><a href="#"><img src="img/img-60x60-1.jpg" alt="" /></a></li>
+            <li><a href="#"><img src="img/img-60x60-1.jpg" alt="" /></a></li>
+            <li><a href="#"><img src="img/img-60x60-1.jpg" alt="" /></a></li>
+            <li><a href="#"><img src="img/img-60x60-1.jpg" alt="" /></a></li>
+            <li><a href="#"><img src="img/img-60x60-1.jpg" alt="" /></a></li>
+            <li><a href="#"><img src="img/img-60x60-1.jpg" alt="" /></a></li>
+            <li><a href="#"><img src="img/img-60x60-1.jpg" alt="" /></a></li>
+            <li><a href="#"><img src="img/img-60x60-1.jpg" alt="" /></a></li>
+            <li><a href="#"><img src="img/img-60x60-1.jpg" alt="" /></a></li>
+            <li><a href="#"><img src="img/img-60x60-1.jpg" alt="" /></a></li>
+            <li><a href="#"><img src="img/img-60x60-1.jpg" alt="" /></a></li>
+            <li><a href="#"><img src="img/img-60x60-1.jpg" alt="" /></a></li>
+        </ul>
+    </div>
+</div> 
+<!-- /lp-container -->
+<?php
+	include_once("footer.php");
+?>
